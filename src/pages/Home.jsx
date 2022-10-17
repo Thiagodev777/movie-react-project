b7a9f70adc30c6e300439f74ae45240387343eaa
Antonio Thiago <ladeiragrande777@gmail.com>
2022-10-16 21:16:15 -0300

    Configurações de Key de api e variaveis de Ambiente

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 
 import { useState, useEffect } from 'react'
 
+const moviesURL = import.meta.env.VITE_API;
+const apiKey = import.meta.env.VITE_API_KEY;
+
 const Home = () => {
   return (
     <div>
