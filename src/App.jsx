7265923923d1e0e92f960dd73e72cf39b667c109
Antonio Thiago <ladeiragrande777@gmail.com>
2022-10-16 21:12:55 -0300

    Configurações e definição do Css inicial + navbar_Component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
-import { Link, Outlet } from 'react-router-dom';
+import { Outlet } from 'react-router-dom';
 import './App.css';
+
+// Components
+import Navbar from './Components/Navbar';
 
 function App() {
   return (
     <div className='App'>
-      <nav id='navbar'>
-        <h2> <Link to="/">moviesLib</Link> </h2>
-        <h2> <Link to="movie/1">movie</Link> </h2>
-        <h2> <Link to="/search">search</Link> </h2>
-      </nav>
-
+      <Navbar/>
       <Outlet />
     </div>
   )
