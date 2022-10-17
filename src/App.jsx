8bd3844b3c62fe4d70e96510894c384a05625d07
Antonio Thiago <ladeiragrande777@gmail.com>
2022-10-16 21:49:45 -0300

    requisições a API e configuração da estrutura central

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import './App.css';
 
 // Components
 import Navbar from './Components/Navbar';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className='App'>
       <Navbar/>
+      <Home />
       <Outlet />
     </div>
   )
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,11 +6,28 @@ const moviesURL = import.meta.env.VITE_API;
 const apiKey = import.meta.env.VITE_API_KEY;
 
 const Home = () => {
+  const [topMovies, setTopMovies] = useState([])
+
+  const getTopRatedMovies = async (url) => {
+    const res = await fetch(url)
+    const data = await res.json()
+    setTopMovies(data.results);
+  }
+
+  useEffect(()=>{
+    const topRatedUrl = `${moviesURL}top_rated?${apiKey}`;
+    getTopRatedMovies(topRatedUrl)
+  }, []);
+
   return (
-    <div>
-      
+    <div className='container'>
+      <h2 className='title'>Melhores filmes:</h2>
+       <div className="movies-container">
+        {topMovies.length === 0 && <p>Carregando...</p>}
+       {topMovies.length > 0 && topMovies.map((movie)=>(<p>{movie.title}</p>))}
+       </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
